2a1640aac46776a3a7cba74766342daf2263d640
yeomsungpil <present7256@gmail.com>
2023-07-25 16:02:20 +0900

    Initial Commit

---
+++ emotionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emotionProject
+//
+//  Created by 염성필 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
